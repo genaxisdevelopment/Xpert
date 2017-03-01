efd027bc0853bd982716e95d9bbb2574efb7c921
Mohammad Wariszan Abdullah Wafa <ibnuabdullahwafa@gmail.com>
2017-03-01 10:51:28 +0800

    Initial Commit

---
+++ Xpert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xpert
+//
+//  Created by Muhammad Wariszan Wildan Che Abdullah Wafa on 01/03/2017.
+//  Copyright © 2017 Genaxis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
